32417b7cff9dbf70b37660d02137e7a3450b8b76
Thales <thalesalves44@gmail.com>
2019-06-15 10:26:33 -0300

    Requisição post sucedida

--- teste_app/lib/form.dart
+++ teste_app/lib/form.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:teste_app/pessoa.dart';
 import 'package:teste_app/post.dart';
 import 'curso.dart';
 class FormDadosPessoais extends StatefulWidget {
@@ -11,20 +12,23 @@ class FormDadosPessoais extends StatefulWidget {
 
 class _FormDadosPessoaisState extends State<FormDadosPessoais> {
 
-  Future<Post> createPost(String url, {Map body}) async {
+  Future<Pessoa> createPost(String url, {Map body}) async {
     return http.post(url, body: body).then((http.Response response) {
       final int statusCode = response.statusCode;
 
       if (statusCode < 200 || statusCode > 400 || json == null) {
         throw new Exception("Error while fetching data");
       }
-      return Post.fromJson(json.decode(response.body));
+      return Pessoa.fromJson(json.decode(response.body));
     });
   }
 
   static final CREATE_POST_URL = 'https://hackathon-uni.herokuapp.com/api/aluno/';
-  TextEditingController titleControler = new TextEditingController();
-  TextEditingController descricaoControler = new TextEditingController();
+  TextEditingController nomeController = new TextEditingController();
+  TextEditingController cpfController = new TextEditingController();
+  TextEditingController rgController = new TextEditingController();
+  TextEditingController sexoController = new TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -48,33 +52,41 @@ class _FormDadosPessoaisState extends State<FormDadosPessoais> {
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: new TextField(
-                        controller: titleControler,
+                        controller: nomeController,
                         decoration: new InputDecoration(labelText: "Nome"),
                       ),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: new TextField(
-                        controller: descricaoControler,
-                        keyboardType: TextInputType.text,
+                        controller: cpfController,
+                        keyboardType: TextInputType.number,
                         decoration: new InputDecoration(labelText: "CPF"),
                       ),
                     ),
-//                    Padding(
-//                      padding: const EdgeInsets.all(8.0),
-//                      child: new TextField(
-//                        keyboardType: TextInputType.number,
-//                        decoration: new InputDecoration(labelText: "RG"),
-//                      ),
-//                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new TextField(
+                        controller: rgController,
+                        keyboardType: TextInputType.number,
+                        decoration: new InputDecoration(labelText: "RG"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new TextField(
+                        controller: sexoController,
+                        decoration: new InputDecoration(labelText: "Sexo"),
+                      ),
+                    ),
                     new RaisedButton(
                         onPressed: () async {
-                          Curso newPost = new Curso(id: 123,
-                              nome: titleControler.text, descricao: descricaoControler.text);
-                          Post p = await createPost(CREATE_POST_URL,
+                          Pessoa newPost = new Pessoa(id: 123,
+                              nome: nomeController.text, cpf: cpfController.text, rg: rgController.text, sexo: sexoController.text);
+                          Pessoa p = await createPost(CREATE_POST_URL,
 
                               body: newPost.toMap());
-                          print(p.title);
+                          print(p.nome);
                       }
                     )
                   ],
---
+++ teste_app/lib/pessoa.dart
@@ -0,0 +1,32 @@
+class Pessoa{
+  final int id;
+  final String nome;
+  final String cpf;
+  final String rg;
+  final String sexo;
+
+
+  Pessoa({this.id,this.nome,this.cpf, this.rg, this.sexo});
+
+  factory Pessoa.fromJson(Map<String, dynamic> json) {
+    return Pessoa(
+      id: json['id'] as int,
+      nome: json['nome'] as String,
+      cpf: json['cpf'] as String,
+      rg: json['rg'] as String,
+      sexo: json['sexo'] as String,
+
+    );
+  }
+  Map toMap() {
+    var map = new Map<String, dynamic>();
+
+    map["nome"] = nome;
+    map["cpf"] = cpf;
+    map["rg"] = rg;
+    map["sexo"] = sexo;
+
+    return map;
+  }
+
+}
